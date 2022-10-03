817121c7d4ff7b89926702679282b2292e5c8ae1
Xv Qian <houlairen@outlook.com>
2022-10-03 11:25:22 +0800

    todo

--- codes/format_jnbb.py
+++ codes/format_jnbb.py
@@ -16,12 +16,34 @@ sheet_dst = wb_dst.active
 rows = sheet_src.max_row
 #print(rows)
 
+key_dict = { '赵固堆派出所':'赵堌堆乡', 
+             '馆驿派出所':'馆驿镇' }
+
 for rid in range(4,rows+1):
     for cid in range(1, 13): #col: A-L
         sheet_dst.cell(rid,cid).value = sheet_src.cell(rid,cid).value
         #print('%d\t%s' %(rid, sheet_src.cell(rid,cid).value))
-    for cid in range(13, 24): #col: A-L
+    for cid in range(13, 20): #col: A-L
         sheet_dst.cell(rid,cid+1).value = sheet_src.cell(rid,cid).value
+    
+    # 判断管控措施 T20 ==> U21
+    find_flag = False
+    value = sheet_src.cell(rid,17).value
+
+    for k, v in key_dict.items():
+        if k in value:
+            tid = tid+1
+            find_flag = True
+            sheet_dst.cell(tid,1).value = v
+            break
+    
+    if not find_flag:
+        continue
+
+
+    for cid in range(20, 24): #col: A-L
+        sheet_dst.cell(rid,cid+1).value = sheet_src.cell(rid,cid).value
+
 
 #cells = sheet_src[4]
 
