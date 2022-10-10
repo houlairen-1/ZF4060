886c855b490a736deba7854760e7b46f39306f3b
Xv Qian <houlairen@outlook.com>
2022-10-10 15:45:07 +0800

    fxq

--- codes/format_fxq.py
+++ codes/format_fxq.py
@@ -4,9 +4,19 @@
 import openpyxl
 import os
 import re
+import logging
+
 
 xlsx_path = r'../data/src/fxq_src.xlsx'
 fn_path = '../data/dst/fxq.txt'
+log_path = '../fxq.log'
+
+
+logging.basicConfig(level=logging.ERROR,
+                    filename=log_path,
+                    format="%(asctime)s - %(name)s - %(levelname)-9s - %(filename)-8s : %(lineno)s line - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S"
+                    )
 
 
 # open file
@@ -20,16 +30,22 @@ with open(fn_path, 'w') as f:
     
     for rid in range(2,rows+1):
         province = sheet.cell(rid,1).value
+        if province:
+            cur_province = province
         city = sheet.cell(rid,2).value
         county = sheet.cell(rid,3).value
-        if province in municipality:
-            city = province
+        if cur_province in municipality:
+            city = cur_province
             county = sheet.cell(rid,2).value
         # split column
+        if not county:
+            print('[ERROR]county is none:{}\t{}\t{}\n'.format(cur_province, city, county))
+            logging.error('county is none:{}\t{}\t{}\n'.format(cur_province, city, county))
+            continue
         county = county.replace('；','\n\t\t\t')
-        print('中风险\t%s\t%s\t%s\n' 
-              %( province, city, county ) )
+        #print('中风险\t%s\t%s\t%s\n' 
+        #      %( cur_province, city, county ) )
 
         f.write('中风险\t%s\t%s\t%s\n' 
-                %( province, city, county ) )
+                %( cur_province, city, county ) )
 
