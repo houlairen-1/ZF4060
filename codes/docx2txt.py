3da6e14d586575e7f2c470e3d27d376f2456d26d
Kylin <houlairen@outlook.com>
2022-10-28 15:46:49 +0800

    docx to txt

---
+++ codes/docx2txt.py
@@ -0,0 +1,85 @@
+#!/usr/local/bin/python3
+#_*_ coding:utf-8 _*_
+
+################################################
+#
+# 2022-10-28 transfer docx to text
+#
+################################################
+
+
+import sys
+import os
+import re
+import docx
+import datetime
+
+today = datetime.datetime.now()
+today_pattern = '.*{}年{}月{}日.*\.docx'.format(today.year,
+                                                today.month,
+                                                today.day)
+# find goal: today's docx
+docx_dir = '../data/src/fxq/docx/'
+txt_dir = '../data/src/fxq/'
+
+txt_fn = 'today.txt'
+
+docx_list = [x for x in os.listdir(docx_dir) if os.path.splitext(x)[1] == '.docx' and re.search(today_pattern, x) ] # 罗列目录内所有xlsx文件
+if len(docx_list) == 0:
+    print('Error:\tdocx donot exit.')
+    sys.exit(1)
+print('Transfer %s to text' %(docx_list[0]) )
+
+docx_path = docx_dir+docx_list[0]
+
+file_h = docx.Document(docx_path)
+
+txt_path = txt_dir+txt_fn
+with open(txt_path, 'w') as f:
+    duplicate = ''
+    duplicate_id = 0
+    for p in file_h.paragraphs:
+        #print(p.text)
+        # 根据空格切割
+        ret = p.text.split(' ')
+        value = ret[0]
+
+        # 去重
+        if len(value) == 0 or value == duplicate:
+            duplicate_id = duplicate_id+1
+            continue
+        duplicate = value
+        f.write('%s\n' %(value) )
+    print('Have writen to %s\n \t delete-duplicate-lines:\t%04d'
+          %(txt_path, duplicate_id) )
+
+
+# 将中高风险区分开
+txt_arr = [ ['高风险', 'high.txt'],
+            ['中风险', 'mid.txt'],
+            ['低风险', 'low.txt'] ]
+
+with open(txt_path, 'r') as f:
+    contents = f.readlines()
+
+for tid in range(len(txt_arr)-1):
+    txt_start_pattern = '^%s区\(\d+\)个' %txt_arr[tid][0]
+    txt_end_pattern = '^%s区\(\d+\)个' %txt_arr[tid+1][0]
+    txt_sub_path = txt_dir + txt_arr[tid][1]
+    with open(txt_sub_path, 'w') as f:
+        start_f = False
+        for value in contents:
+            ret = re.search(txt_start_pattern, value)
+            if ret:
+                start_f = True
+                continue
+            ret = re.search(txt_end_pattern, value)
+            if ret:
+                end_f = True
+                break
+            if not start_f: # 找到起点
+                continue
+
+            f.write('%s' %(value) )
+            
+    print('%s has generated.' %(txt_sub_path))
--- codes/join.py
+++ codes/join.py
@@ -5,19 +5,15 @@
 #
 # 2022-10-21 处理北京
 #
+# 2022-10-28 处理中高两个文件
+#
 ################################################
 
 
 import openpyxl
 import os
 import re
-
-xlsx_path = r'../data/template_risk.xlsx'
-fn_path = '../data/src/fxq/test.txt'
-
-fn_list = []
-stack = [] #风险区栈 拼接省-市-县
-pattern = r'(.+)\(\d+\)个'
+import datetime
 
 # 23 + 5 + 4 + 2
 province = [ '河北省', '山西省', '辽宁省', '吉林省', 
@@ -37,86 +33,105 @@ municipality = {'天津市', '上海市', '重庆市'}
 # normal
 capital = { '北京市' }
 
-# open file
-wb = openpyxl.load_workbook(xlsx_path)
-sheet = wb['Sheet1']
-rid = 2 # sheet record id
+txt_arr = [ ['高风险', 'high.txt'],
+            ['中风险', 'mid.txt'] ]
 
-with open(fn_path, 'r') as f:
-    fn_list = f.readlines()
+for tid in range(len(txt_arr)):
 
-    #print(fn_list)
+    xlsx_path = r'../data/template_risk.xlsx'
+    fn_path = '../data/src/fxq/%s' %(txt_arr[tid][1])
     
-for value in fn_list:
-    value = value.strip('\n')
-    ret = re.search(pattern, value)
-    if ret: # 1st if : 处理带括号的数据
-        # 判断省和市
-        key = ret.group(1)
-        if key in province: # 2nd if : 判断省
-            if len(stack)==0: # 3rd if : 判断是否到了下一省份
-                #print(key)
-                stack.append(key)
-            else:
-                #到了下一省份
-                if len(stack) != 2:
-                    print('stack Error!%s' %(stack) )
-                    break
-                stack.pop() #弹出市
-                stack.pop() #弹出省
-                stack.append(key) #new province in stack
-        elif key in municipality: # 2nd if : 判断直辖市
-            if len(stack)==0:
-                #print(key)
-                stack.append(key)
-                stack.append(key)
-            else:
-                #到了下一省份
-                if len(stack) != 2:
-                    print('stack Error!%s' %(stack) )
-                    break
-                stack.pop() #弹出市
-                stack.pop() #弹出省
-                stack.append(key) #new province in stack
-                stack.append(key) #new province in stack
-            
-        elif key in capital: # 2nd if : 判断直辖市
-            if len(stack)==0:
-                #print(key)
-                stack.append(key)
-                stack.append(key)
-            else:
-                #到了下一省份
-                if len(stack) != 2:
-                    print('stack Error!%s' %(stack) )
-                    break
-                stack.pop() #弹出市
-                stack.pop() #弹出省
-                stack.append(key) 
-                stack.append(key) 
-        else: # 这是市级行政区
-            if len(stack) == 2 and stack[-2] not in capital:
-                stack.pop()
-            #print('\t%s' %(key) )
-            
-            if stack[-1] not in capital:
-                stack.append(key)
-            else:
-                county = key
-                #print('key is %s' %(key))
-        continue
-    #print(value)
+    fn_list = []
+    stack = [] #风险区栈 拼接省-市-县
+    pattern = r'(.+)\(\d+\)个'
     
 
-    rid = rid+1
-    sheet.cell(rid, 2).value = stack[-2] # col B
-    sheet.cell(rid, 3).value = stack[-1] # col C
-    if stack[-2] in capital:
-        sheet.cell(rid, 4).value = county # col D
-        print('%s\t%s\t%s' %(stack[-2], stack[-1], county) )
-    else:
-        sheet.cell(rid, 4).value = value # col D
-        print('%s\t%s\t%s' %(stack[-2], stack[-1], value) )
+    # open file
+    wb = openpyxl.load_workbook(xlsx_path)
+    sheet = wb['Sheet1']
+    rid = 2 # sheet record id
+    
+    with open(fn_path, 'r') as f:
+        fn_list = f.readlines()
+    
+        #print(fn_list)
+        
+    for value in fn_list:
+        value = value.strip('\n')
+        ret = re.search(pattern, value)
+        if ret: # 1st if : 处理带括号的数据
+            # 判断省和市
+            key = ret.group(1)
+            if key in province: # 2nd if : 判断省
+                if len(stack)==0: # 3rd if : 判断是否到了下一省份
+                    #print(key)
+                    stack.append(key)
+                else:
+                    #到了下一省份
+                    if len(stack) != 2:
+                        print('stack Error!%s' %(stack) )
+                        break
+                    stack.pop() #弹出市
+                    stack.pop() #弹出省
+                    stack.append(key) #new province in stack
+            elif key in municipality: # 2nd if : 判断直辖市
+                if len(stack)==0:
+                    #print(key)
+                    stack.append(key)
+                    stack.append(key)
+                else:
+                    #到了下一省份
+                    if len(stack) != 2:
+                        print('stack Error!%s' %(stack) )
+                        break
+                    stack.pop() #弹出市
+                    stack.pop() #弹出省
+                    stack.append(key) #new province in stack
+                    stack.append(key) #new province in stack
+                
+            elif key in capital: # 2nd if : 判断直辖市
+                if len(stack)==0:
+                    #print(key)
+                    stack.append(key)
+                    stack.append(key)
+                else:
+                    #到了下一省份
+                    if len(stack) != 2:
+                        print('stack Error!%s' %(stack) )
+                        break
+                    stack.pop() #弹出市
+                    stack.pop() #弹出省
+                    stack.append(key) 
+                    stack.append(key) 
+            else: # 这是市级行政区
+                if len(stack) == 2 and stack[-2] not in capital:
+                    stack.pop()
+                #print('\t%s' %(key) )
+                
+                if stack[-1] not in capital:
+                    stack.append(key)
+                else:
+                    county = key
+                    #print('key is %s' %(key))
+            continue
+        #print(value)
+        
+    
+        rid = rid+1
+        sheet.cell(rid, 1).value = txt_arr[tid][0] # col A
+        sheet.cell(rid, 2).value = stack[-2] # col B
+        sheet.cell(rid, 3).value = stack[-1] # col C
+        if stack[-2] in capital:
+            sheet.cell(rid, 4).value = county # col D
+            #print('%s\t%s\t%s' %(stack[-2], stack[-1], county) )
+        else:
+            sheet.cell(rid, 4).value = value # col D
+            #print('%s\t%s\t%s' %(stack[-2], stack[-1], value) )
 
-
-wb.save('../data/dst/risk.xlsx')
+    today = datetime.datetime.now()
+    dst_fn = '%s_%04d-%02d-%02d.xlsx' %( txt_arr[tid][0],
+                                         today.year,
+                                         today.month,
+                                         today.day)
+    wb.save('../data/dst/%s' %(dst_fn) )
+    print('%s has generated. \tTotal %d records.' %(dst_fn, rid) )
