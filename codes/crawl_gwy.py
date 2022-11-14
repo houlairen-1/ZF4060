f001c830252c3c059846fd8b700dc94c4f2f04e0
Xu Qian <houlairen@outlook.com>
2022-11-14 12:41:39 +0800

    'debug'

--- codes/crawl_gwy.py
+++ codes/crawl_gwy.py
@@ -6,23 +6,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as Expect
 import re
+import openpyxl
+import datetime
+from time import sleep
 
 
 MAX_PAGES = 100
 NUM_BUTTONS = 9
 
-browser = webdriver.Chrome(executable_path="../drivers/chromedriver")
+#browser = webdriver.Chrome(executable_path="../drivers/chromedriver") #mac
+browser = webdriver.Chrome(executable_path="../drivers/chromedriver.exe") #windows
 browser.get('https://bmfw.www.gov.cn/yqfxdjcx/risk.html')
-browser.implicitly_wait(60)
-#Wait(browser, 60).until(
+browser.implicitly_wait(30)
+#Wait(browser, 30).until(
 #    Expect.presence_of_element_located((By.CLASS_NAME, "risk-info-table"))
 #)
 
-risk_levels_list = [ '中风险', '中风险' ]
+risk_levels_list = [ '中风险' ]
 
+f = open('../data/dst/test.txt', 'w')
 for level in risk_levels_list:
     # 等待加载
-
+    xlsx_path = r'../data/template_risk.xlsx'
+    f.write('Flag:\t%s\n' %(level) )
+    # open file
+    wb = openpyxl.load_workbook(xlsx_path)
+    sheet = wb['Sheet1']
+    rid = 2 # sheet record id
+    
     # eg.中风险区 （2155）
     level_pattern = ' %s区 （\d+）' %(level)
     risk_levels = browser.find_elements(By.CLASS_NAME, 'tabs-header-tab')
@@ -45,7 +56,7 @@ for level in risk_levels_list:
 
     # visit all pages
     for pid in range(1, MAX_PAGES):
-    
+        sleep(3)
         btn = browser.find_elements(By.CSS_SELECTOR, 'div.pages-box > button')
         # find pid in 9 buttons
         # bid: 0 is 首页, 1 is 上一页
@@ -66,8 +77,19 @@ for level in risk_levels_list:
         
         risk_table = browser.find_elements(By.CLASS_NAME, "risk-info-table")
         for risk_title in risk_table:
-            flight_price_row = risk_title.find_element(By.CLASS_NAME, "risk-info-table-title")
-            print(flight_price_row.get_attribute("textContent"))
+            # body > div.content > div.container > div:nth-child(2) > div.risk-tables > div:nth-child(10) > div > div:nth-child(1)
+            # /html/body/div[2]/div[2]/div[2]/div[2]/div[10]/div/div[1] xpath
+            # <div>山西省 晋城市 城区</div>
+            risk_row = risk_title.find_element(By.CLASS_NAME, "risk-info-table-title")
+            record_txt = risk_row.get_attribute("textContent")
+            print(risk_row.get_attribute("textContent"))
+            #record = risk_row.get_attribute("textContent")
+            #record_html = risk_row.get_attribute("innerHTML")
+            #record_pattern = '<div>(.*)<div><div.*>(.*)</div>'
+            #record_txt = re.match(record_pattern, record_html).group(1)
+            #print(record_txt)
+            f.write('%s\n' %(record_txt))
+            
             # single page
     
     
