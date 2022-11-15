4921cbe4f826a43a910854fe2a9133a8d098099b
Xu Qian <houlairen@outlook.com>
2022-11-15 10:43:41 +0800

    m auto crawl

--- codes/crawl_gwy.py
+++ codes/crawl_gwy.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as Expect
+from selenium.webdriver.chrome.service import Service
 import re
 import openpyxl
 import datetime
@@ -15,17 +16,29 @@ MAX_PAGES = 100
 NUM_BUTTONS = 9
 
 #browser = webdriver.Chrome(executable_path="../drivers/chromedriver") #mac
-browser = webdriver.Chrome(executable_path="../drivers/chromedriver.exe") #windows
+# browser = webdriver.Chrome(executable_path="../drivers/chromedriver.exe") #windows
+# Warning:  DeprecationWarning: executable_path has been deprecated, please pass in a Service object 版本过时
+options = webdriver.ChromeOptions()
+# solve SSL 证书错误问题
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+
+# ignore useless log
+options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
+
+ser = Service("../drivers/chromedriver.exe") #windows
+browser = webdriver.Chrome(service=ser, options=options)
 browser.get('https://bmfw.www.gov.cn/yqfxdjcx/risk.html')
-browser.implicitly_wait(30)
+browser.implicitly_wait(5)
 #Wait(browser, 30).until(
 #    Expect.presence_of_element_located((By.CLASS_NAME, "risk-info-table"))
 #)
 
-risk_levels_list = [ '中风险' ]
+risk_levels_list = [ '高风险', '中风险' ]
 
-f = open('../data/dst/test.txt', 'w')
 for level in risk_levels_list:
+    f = open('../data/src/fxq/crawl_%s.txt' %(level), 'w')
+
     # 等待加载
     xlsx_path = r'../data/template_risk.xlsx'
     f.write('Flag:\t%s\n' %(level) )
@@ -56,7 +69,7 @@ for level in risk_levels_list:
 
     # visit all pages
     for pid in range(1, MAX_PAGES):
-        sleep(3)
+        sleep(2)
         btn = browser.find_elements(By.CSS_SELECTOR, 'div.pages-box > button')
         # find pid in 9 buttons
         # bid: 0 is 首页, 1 is 上一页
@@ -77,21 +90,38 @@ for level in risk_levels_list:
         
         risk_table = browser.find_elements(By.CLASS_NAME, "risk-info-table")
         for risk_title in risk_table:
-            # body > div.content > div.container > div:nth-child(2) > div.risk-tables > div:nth-child(10) > div > div:nth-child(1)
-            # /html/body/div[2]/div[2]/div[2]/div[2]/div[10]/div/div[1] xpath
             # <div>山西省 晋城市 城区</div>
             risk_row = risk_title.find_element(By.CLASS_NAME, "risk-info-table-title")
-            record_txt = risk_row.get_attribute("textContent")
-            print(risk_row.get_attribute("textContent"))
+            #record_txt = risk_row.get_attribute("textContent")
+            #print(risk_row.get_attribute("textContent"))
             #record = risk_row.get_attribute("textContent")
-            #record_html = risk_row.get_attribute("innerHTML")
-            #record_pattern = '<div>(.*)<div><div.*>(.*)</div>'
-            #record_txt = re.match(record_pattern, record_html).group(1)
+            record_html = risk_row.get_attribute("innerHTML")
+            record_pattern = '<div>(.*)</div><div.*>(.*)</div>'
+            try:
+                record_txt = re.match(record_pattern, record_html).group(1)
+            except:
+                print('[ERROR] Pattern does not match.\t%s' %(risk_row.get_attribute("textContent")))
+                continue
             #print(record_txt)
             f.write('%s\n' %(record_txt))
-            
+            record_arr = record_txt.split(' ')
+            rid = rid+1
+            sheet.cell(rid, 1).value = level
+            for tid in range(len(record_arr)):
+                sheet.cell(rid, tid+2).value = record_arr[tid]
             # single page
+            
     
     
     if pid == MAX_PAGES:
         print('MAX_PAGES is smaller.')
+
+    f.close()
+    today = datetime.datetime.now()
+    dst_fn = '%s_%04d-%02d-%02d.xlsx' %( level,
+                                         today.year,
+                                         today.month,
+                                         today.day)
+
+    wb.save('../data/dst/%s' %(dst_fn))
+    print('%s has generated.' %(dst_fn))
